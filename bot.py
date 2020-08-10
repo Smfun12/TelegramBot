7ac9d843d45672dc92646ccef4c3943d8c74f192
Sasha <sasharesh12@gmail.com>
2020-08-10 13:28:31 +0300

    db.db

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def start_cmd_handler(message: types.Message):
-    if not db.subscriber_exists(message.from_user.username):
+    if not db.subscriber_exists(message.from_user.id):
         db.add_subscriber(message.from_user.username)
     else:
         db.update_subscription(message.from_user.id, True)
