00379923474fc33e6517d7913f7eb17cd93a7d2d
Sasha <sasharesh12@gmail.com>
2020-08-13 14:17:44 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -91,6 +91,8 @@ async def open_site(message: types.Message):
 
 @dp.message_handler(commands='followers')
 async def show_followers(message: types.Message):
+    if message.from_user.username == "sasha_reshetar":
+        await message.answer(str(user_dictionary))
     await message.answer(str(len(user_dictionary)))
 
 
